6863b19379fab49e728cf2631b1abb65dd5a7eb9
Uni-lux <douglasjose807gg@gmail.com>
2025-07-20 01:20:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function mostrarFormulario(tipo) {
-  document.getElementById("form-cliente").style.display = "none";
-  document.getElementById("form-profissional").style.display = "none";
-  document.getElementById("form-sobre").style.display = "none";
-
-  if (tipo === 'cliente') {
-    document.getElementById("form-cliente").style.display = "block";
-  } else if (tipo === 'profissional') {
-    document.getElementById("form-profissional").style.display = "block";
-  } else if (tipo === 'sobre') {
-    document.getElementById("form-sobre").style.display = "block";
-  }
-}
+document.getElementById('menu-toggle').addEventListener('click', () => {
+  document.getElementById('sidebar').classList.toggle('open');
+});
