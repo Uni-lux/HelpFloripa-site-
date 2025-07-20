bc796d336abbc05eee5ae1e03f08bfc840a56a4c
Uni-lux <douglasjose807gg@gmail.com>
2025-07-19 21:21:41 -0300

    Update script.js

--- css/js/script.js
+++ css/js/script.js
@@ -1,10 +1,13 @@
-function mostrarSecao(secaoId) {
-  const secoes = document.querySelectorAll('.secao');
-  secoes.forEach(secao => secao.style.display = 'none');
+function mostrarFormulario(tipo) {
+  document.getElementById("form-cliente").style.display = "none";
+  document.getElementById("form-profissional").style.display = "none";
+  document.getElementById("form-sobre").style.display = "none";
 
-  const secaoSelecionada = document.getElementById(secaoId);
-  if (secaoSelecionada) {
-    secaoSelecionada.style.display = 'block';
-    secaoSelecionada.scrollIntoView({ behavior: 'smooth' });
+  if (tipo === 'cliente') {
+    document.getElementById("form-cliente").style.display = "block";
+  } else if (tipo === 'profissional') {
+    document.getElementById("form-profissional").style.display = "block";
+  } else if (tipo === 'sobre') {
+    document.getElementById("form-sobre").style.display = "block";
   }
 }
