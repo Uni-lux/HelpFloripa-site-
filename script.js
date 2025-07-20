0ffd32e3d03f89ff15fa3317070f5ac06134d648
Uni-lux <douglasjose807gg@gmail.com>
2025-07-20 03:33:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Seleciona o botão e a barra lateral
 const toggleBtn = document.querySelector('.toggle-btn');
 const sidebar = document.querySelector('.sidebar');
 
-// Adiciona o evento de clique para alternar a classe 'active'
 toggleBtn.addEventListener('click', () => {
   sidebar.classList.toggle('active');
 });
